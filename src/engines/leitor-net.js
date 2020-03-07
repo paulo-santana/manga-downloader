122355d7d75376fc36041d677c498fe93e0d93bb
Paulo Santana <paulo.santana.r@gmail.com>
2020-03-07 18:10:50 -0300

    feat: implement image downloading and saving

--- src/engines/leitor-net.js
+++ src/engines/leitor-net.js
@@ -1,26 +1,145 @@
 const axios = require('axios').default;
 const FormData = require('form-data');
+const Fs = require('fs');
+const Path = require('path');
 
 const leitorNet = {
+  config: {
+    searchUrl: 'https://leitor.net/lib/search/series.json',
+    listChaptersUrl: 'https://leitor.net/series/chapters_list.json',
+    chapterPagesUrl: 'https://leitor.net/leitor/pages/',
+  },
   async searchManga(searchTerm) {
     const form = new FormData();
     form.append('search', searchTerm);
 
-    const result = await axios({
-      method: 'post',
-      headers: {
-        'content-type': 'aaplication/x-www-form-urlencoded',
-        'X-Requested-With': 'XMLHttpRequest',
-        ...form.getHeaders(),
-      },
-      url: 'https://leitor.net/lib/search/series.json',
-      data: form,
-    });
-    const mangas = result.data.series.map(manga => ({
-      id: manga.id_serie,
-      title: manga.name,
-    }));
-    return mangas;
+    try {
+      const result = await axios({
+        method: 'post',
+        headers: {
+          'content-type': 'aaplication/x-www-form-urlencoded',
+          'X-Requested-With': 'XMLHttpRequest',
+          ...form.getHeaders(),
+        },
+        url: this.config.searchUrl,
+        data: form,
+      });
+      const mangas = result.data.series.map(manga => ({
+        id: manga.id_serie,
+        title: manga.name,
+      }));
+      return mangas;
+    } catch (error) {
+      console.error('DEU RUIM', error);
+      return false;
+    }
+  },
+  async getChapters(mangaId) {
+    const chapters = [];
+    let page = 1;
+    let response = {};
+    try {
+      do {
+        response = await axios({
+          method: 'get',
+          headers: { 'X-Requested-With': 'XMLHttpRequest' },
+          url: `${this.config.listChaptersUrl}?page=${page}&id_serie=${mangaId}`,
+        });
+
+        if (response.data.chapters) {
+          chapters.push(...response.data.chapters);
+          page++;
+        }
+      } while (response.data.chapters);
+      return chapters.reverse();
+    } catch (error) {
+      console.error(error.response.data);
+      return false;
+    }
+  },
+
+  async downloadAll(chapters) {
+    const length = chapters.length;
+    for (let i = 0; i < length; i++) {
+      const chapter = chapters[i];
+      const release = Object.values(chapter.releases)[0];
+
+      const token = await this.getChapterToken(release.link);
+
+      const url = `${this.config.chapterPagesUrl}${release.id_release}.json`;
+
+      const response = await axios({
+        method: 'get',
+        url,
+        params: {
+          key: token,
+        },
+      });
+
+      const { images } = response.data;
+      chapterName = `${chapter.number.padStart(3, '0')} - ${
+        chapter.chapter_name
+      }`;
+      const mangaName = chapter.name;
+      await this.downloadImages(images, chapterName, mangaName);
+      await this.timeout(1000);
+    }
+  },
+
+  async getChapterToken(link) {
+    const chapterPageUrl = `https://leitor.net${link}`;
+    const chapterPage = await axios.get(chapterPageUrl);
+    const token = chapterPage.data.match(
+      new RegExp(/[\&\?]token\=(\w+)\&?/i),
+    )[1];
+    return token;
+  },
+
+  async downloadImages(images, chapterName, manga) {
+    for (const [i, image] of images.entries()) {
+      try {
+        const response = await axios({
+          method: 'get',
+          url: image,
+          responseType: 'stream',
+        });
+
+        pageFullName = image.split('/');
+        const page = pageFullName[pageFullName.length - 1];
+        const path = Path.resolve(
+          __dirname,
+          '..',
+          '..',
+          'mangas',
+          manga,
+          chapterName,
+          page,
+        );
+
+        Fs.mkdirSync(path.slice(0, path.length - page.length), {
+          recursive: true,
+        });
+
+        const writer = Fs.createWriteStream(path);
+        console.log(`salvando página ${page} do capítulo ${chapterName}`);
+
+        response.data.pipe(writer);
+
+        const finish = new Promise((resolve, reject) => {
+          writer.on('finish', resolve);
+          writer.on('error', reject);
+        });
+
+        await this.timeout(500);
+      } catch (error) {
+        console.error(error);
+        process.exit(31);
+      }
+    }
+  },
+
+  timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve), ms);
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import readline from 'readline-sync';
-import leitorNet from './engines/leitor-net';
+const readline = require('readline-sync');
+const leitorNet = require('./engines/leitor-net');
 
 readline.setEncoding('utf8');
 
@@ -8,13 +8,55 @@ async function main() {
 
   const foundMangas = await leitorNet.searchManga(search);
 
+  if (!foundMangas) {
+    console.log('Aconteceu um erro, abortando.');
+    return;
+  }
+
   const index = readline.keyInSelect(
     foundMangas.map(item => item.title),
     'Qual desses você quer baixar?',
   );
   const selectedManga = foundMangas[index];
 
-  const chapters = leitorNet.getChapters(selectedManga.id);
+  const chapters = await leitorNet.getChapters(selectedManga.id);
+  console.log(chapters.map(chapter => chapter.number));
+
+  if (!chapters) {
+    console.log('Aconteceu um erro, abortando.');
+    return;
+  }
+
+  console.log(
+    `Esse mangá vai do capítulo ${chapters[0].number} até ${
+      chapters[chapters.length - 1].number
+    }.`,
+  );
+
+  const downloadOption = readline.keyInSelect(
+    ['Baixar todos', 'Baixar uma faixa de capítulos específica'],
+    'Você quer baiar tudo agora ou quer baixar apenas uma faixa?',
+  );
+
+  switch (downloadOption) {
+    case 0:
+      console.log('Vamos baixar todos então...');
+      await leitorNet.downloadAll(chapters);
+      break;
+    case 1:
+      console.log(
+        'Agora vamos fazer o downloads das faixas de capítulos que você específicar',
+      );
+      const firstChapter = readline.question(
+        'Digite o primeiro capítulo que quer baixar: ',
+      );
+      const lastChapter = readline.question('Agora, digite o último: ');
+      leitorNet.downloadRange(chapters, firstChapter, lastChapter);
+      break;
+    case -1: {
+      console.log('flw mané!');
+    }
+  }
 }
 
 main();
